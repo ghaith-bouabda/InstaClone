bb92f5ac3ee4c3d3618a2a7c107bf9fa00e5a99a
ghaith <ghaithbouabda@gmail.com>
2025-06-27 16:12:29 +0100

    Initial commit

---
+++ Backend/src/main/java/com/example/InstaClone/InstaCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.InstaClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaCloneApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/example/InstaClone/InstaCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InstaClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
